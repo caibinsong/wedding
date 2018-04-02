51c2d0d11536065460cf5ae0053ab44b90004b3e
caibinsong <104217773@qq.com>
2018-04-02 14:10:01 +0800

    wx

--- controllers/wx_controller.go
+++ controllers/wx_controller.go
@@ -104,7 +104,7 @@ func ToJsonAttach(msg string) (int, string, error) {
 		log.Println(err.Error())
 		return 0, "", errors.New("红包不存在")
 	}
-	rst := fmt.Sprintf(`{"chatroomId":%s,"msgType":%s,"userId":%s,"weddingId":%s,"msg":"{\"rp_id\":%s,\"type\":%s,\"wish\":\"%s\","question": "%s"}"}`,
+	rst := fmt.Sprintf(`{"chatroomId":%s,"msgType":%s,"userId":%s,"weddingId":%s,"msg":"{\"rp_id\":%s,\"type\":%s,\"wish\":\"%s\",\"question\": \"%s\"}"}`,
 		arr[4], arr[7], arr[6], arr[5], arr[1], arr[2], arr[3], redpacket.Question)
 	return rp_id, rst, nil
 }
